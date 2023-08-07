f5b0d207b4618af8375740ee36da27b3bfda5f92
Shurikosa <shurikosaula@gmail.com>
2023-08-07 20:38:59 +0300

    bugs fixed

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -2,26 +2,54 @@ import java.util.Scanner;
 
 public class SimpleCalculator {
 
-    static double value1;
-    static char operator;
-    static double value2;
+    private double value1;
+    private char operator;
+    private double value2;
+    private final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Insert first value please:");
-        value1 = scanner.nextDouble();
+    public void run(){
 
-        System.out.println("Insert operation (+, -, /, *): ");
-        operator = scanner.next().charAt(0);
+        boolean isValidInput = false;
+        while(!isValidInput){
+            System.out.println("Insert first value please (must be a number): ");
+            if (scanner.hasNextDouble()){
+                value1 = scanner.nextDouble();
+                isValidInput = true;
+            }else {
+                System.out.println("Incorrect input try again : ");
+                scanner.nextLine();
+            }
+        }
+        isValidInput = false;
 
-        System.out.println("Insert second value");
-        value2 = scanner.nextDouble();
+        while(!isValidInput){
+            System.out.println("Insert operator please (+, -, *, /): ");
+            String input = scanner.next().trim();
+            if (input.length() == 1 && "+-*/".contains(input)){
+               operator = input.charAt(0);
+                isValidInput = true;
+            }else {
+                System.out.println("Incorrect operator try again : ");
+                scanner.nextLine();
+            }
+        }
+        isValidInput = false;
 
-        System.out.println(SimpleCalculator.calculate(value1, operator,value2));
+        while(!isValidInput){
+            System.out.println("Insert second value please (must be a number): ");
+            if (scanner.hasNextDouble()){
+                value2 = scanner.nextDouble();
+                isValidInput = true;
+            }else {
+                System.out.println("Incorrect input try again : ");
+                scanner.nextLine();
+            }
+        }
+        scanner.close();
+
+        System.out.println(calculate( value1, operator, value2));
     }
-
-
 
     public static double calculate(double value1, char operator, double value2 ){
         double result;
